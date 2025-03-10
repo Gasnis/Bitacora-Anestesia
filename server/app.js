60cad8fe0ce06c5581827f9ebed26b1f75db758f
Gasnis <113210850+Gasnis@users.noreply.github.com>
2025-03-10 17:44:37 +0100

    configure CORS to allow requests from any origin

--- server/app.js
+++ server/app.js
@@ -4,7 +4,11 @@ const opsRoutes = require('./routes/ops');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-app.use(cors());
+// Configurar CORS para permitir cualquier origen
+app.use(cors({
+    origin: '*'
+}));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/", opsRoutes);
